4157feb5c2a8401c785f4d97479a239fadbb4182
benbrunyee <brunyeeb@gmail.com>
2025-01-04 02:49:59 +0000

    fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,42 +7,48 @@ type MessageI = {
 	role: 'assistant' | 'user';
 };
 
-export const chat = onCall(async (request) => {
-	const nodeEnv = NODE_ENV.value();
+export const chat = onCall(
+	{
+		cpu: 1,
+		minInstances: 1
+	},
+	async (request) => {
+		const nodeEnv = NODE_ENV.value();
 
-	if (nodeEnv === 'development') {
-		return 'Some dummy text';
-	}
+		if (nodeEnv === 'development') {
+			return 'Some dummy text';
+		}
 
-	const client = new OpenAI({
-		apiKey: OPENAI_API_KEY.value()
-	});
+		const client = new OpenAI({
+			apiKey: OPENAI_API_KEY.value()
+		});
 
-	const updatedMessages = request.data.messages as MessageI[];
+		const updatedMessages = request.data.messages as MessageI[];
 
-	const thread = await client.beta.threads.create({
-		messages: updatedMessages.slice(0, -1)
-	});
+		const thread = await client.beta.threads.create({
+			messages: updatedMessages.slice(0, -1)
+		});
 
-	await client.beta.threads.messages.create(thread.id, {
-		content: updatedMessages[updatedMessages.length - 1].content,
-		role: 'user'
-	});
+		await client.beta.threads.messages.create(thread.id, {
+			content: updatedMessages[updatedMessages.length - 1].content,
+			role: 'user'
+		});
 
-	const run = await client.beta.threads.runs.createAndPoll(thread.id, {
-		assistant_id: 'asst_sDhIBGioG4HJxdLi1RmaUtpl'
-	});
+		const run = await client.beta.threads.runs.createAndPoll(thread.id, {
+			assistant_id: 'asst_sDhIBGioG4HJxdLi1RmaUtpl'
+		});
 
-	if (run.status === 'completed') {
-		const messages = await client.beta.threads.messages.list(run.thread_id);
-		const latestMessage = messages.data[0].content[0];
+		if (run.status === 'completed') {
+			const messages = await client.beta.threads.messages.list(run.thread_id);
+			const latestMessage = messages.data[0].content[0];
 
-		if (latestMessage.type === 'text') {
-			return latestMessage.text.value;
+			if (latestMessage.type === 'text') {
+				return latestMessage.text.value;
+			} else {
+				return 'I am not sure how to respond to that.';
+			}
 		} else {
 			return 'I am not sure how to respond to that.';
 		}
-	} else {
-		return 'I am not sure how to respond to that.';
 	}
-});
+);
